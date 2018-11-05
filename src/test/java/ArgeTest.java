909983e10df6b326483f80aac45c0a353acca19e
GreegAV <greegav@gmail.com>
2018-11-05 17:38:04 +0200

    Tests changed

--- src/test/java/ArgeTest.java
+++ src/test/java/ArgeTest.java
@@ -9,9 +9,15 @@ public class ArgeTest {
     }
     @Test
     public void test1() {
-        System.out.println("Fixed Tests: nbYear");
+        System.out.println("Fixed Test1: nbYear");
         testing(Arge.nbYear(1500, 5, 100, 5000),15);
+    }
+    public void test2() {
+        System.out.println("Fixed Test2: nbYear");
         testing(Arge.nbYear(1500000, 2.5, 10000, 2000000), 10);
+    }
+    public void test3() {
+        System.out.println("Fixed Test3: nbYear");
         testing(Arge.nbYear(1500000, 0.25, 1000, 2000000), 94);
     }
 }
