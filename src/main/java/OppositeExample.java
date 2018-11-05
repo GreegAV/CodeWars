2d8020ddcc392ca6b4a35415f8c1852de1acfa0e
GreegAV <greegav@gmail.com>
2018-11-05 17:50:27 +0200

    3

---
+++ src/main/java/OppositeExample.java
@@ -0,0 +1,20 @@
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+
+
+ */
+public class OppositeExample
+{
+    public static int opposite(int number)
+    {
+        // your code here
+        return number*(-1);
+    }
+}
---
+++ src/test/java/OppositeExampleTests.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class OppositeExampleTests {
+    @Test
+    public void tests() {
+        assertEquals(-1, OppositeExample.opposite(1));
+    }
+}
