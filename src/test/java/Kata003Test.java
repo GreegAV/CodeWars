5b5d872fb5dfa1d58d678950f0c98db35da5db68
GreegAV <greegav@gmail.com>
2018-11-08 10:57:05 +0200

    Rename

--- src/test/java/Kata003Test.java
+++ src/test/java/Kata003Test.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class Kata003Tests {
+public class Kata003Test {
     @Test
     public void tests() {
         assertEquals(-1, Kata003.opposite(1));
