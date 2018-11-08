6711ad3f0663ab5d0ab379548edb9294b1c83be2
GreegAV <greegav@gmail.com>
2018-11-08 15:45:52 +0200

    Kata009

---
+++ src/test/java/Kata009Test.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Kata009Test {
+    @Test
+    public void tests1() {
+        assertEquals("67", Kata009.numberToString(67));
+    }
+    @Test
+    public void tests2() {
+        assertEquals("123", Kata009.numberToString(123));
+    }
+    @Test
+    public void tests3() {
+        assertEquals("999", Kata009.numberToString(999));
+    }
+}
