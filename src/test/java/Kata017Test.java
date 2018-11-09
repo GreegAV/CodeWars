67e041b150e82b10c51dfac0e434b4eaeae69d06
GreegAV <greegav@gmail.com>
2018-11-09 16:13:22 +0200

    Kata017

---
+++ src/test/java/Kata017Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Kata017Test {
+
+    @Test
+    public void test1() {
+        assertEquals(-42, Kata017.makeNegative(42));
+    }
+}
