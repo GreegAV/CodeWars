dc8ceb17823f39f688ec57984042af42c6bfa8e3
GreegAV <greegav@gmail.com>
2018-11-12 17:01:23 +0200

    Kata019

---
+++ src/main/java/kyu8/Kata019.java
@@ -0,0 +1,18 @@
+package kyu8;
+/*
+Kata: Reversed sequence
+Task: Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]
+ */
+
+public class Kata019 {
+    public static int[] reverse(int n){
+        //your code
+        int[] res= new int[n];
+        for (int i = 0; i <n ; i++) {
+            res[i]=n-i;
+        }
+        return res;
+    }
+}
