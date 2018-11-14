ab99df64ca0a134727ebd1f6812b0ff71bf8d227
GreegAV <greegav@gmail.com>
2018-11-14 16:27:36 +0200

    Kata029

--- src/test/java/kyu8/Kata029Test.java
+++ src/test/java/kyu8/Kata029Test.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-// TODO: Replace examples and use TDD development by writing your own tests
-
 public class Kata029Test {
 
     @Test
