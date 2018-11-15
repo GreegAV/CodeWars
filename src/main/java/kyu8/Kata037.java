12bdc7f9c3f9d39db8bfe6559b2ce01e2d08607c
GreegAV <greegav@gmail.com>
2018-11-15 17:52:41 +0200

    Kata038

--- src/main/java/kyu8/Kata037.java
+++ src/main/java/kyu8/Kata037.java
@@ -5,7 +5,6 @@ Task: Add two logs with base X, with the value of A and B. Example log A + log B
  */
 public class Kata037 {
     public static double logs(double x, double a, double b) {
-        // TODO add implementation
         return Math.log10(a)/Math.log10(x)+Math.log10(b)/Math.log10(x);
     }
 }
