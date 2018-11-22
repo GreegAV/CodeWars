51eb9a27780a692449656df086c844443ba86fe5
Андрей Григорьев <greegav@gmail.com>
2018-11-22 12:52:40 +0200

    Kata062

---
+++ src/main/java/kyu8/Kata062.java
@@ -0,0 +1,11 @@
+package kyu8;
+/*
+Kata: You Can't Code Under Pressure #1
+Task: Code as fast as you can! You need to double the integer and return it.
+ */
+public class Kata062 {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i*2;
+    }
+}
---
+++ src/test/java/kyu8/Kata062Test.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Kata062Test {
+    @Test
+    public final void test_all() {
+        Assert.assertEquals("Nope!", 4, Kata062.doubleInteger(2)); // Test cases here!
+    }
+}
