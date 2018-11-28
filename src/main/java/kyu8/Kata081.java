9b502d8554fe275af47b590fd772084cf8e55a90
Андрей Григорьев <greegav@gmail.com>
2018-11-28 18:03:08 +0200

    Kata081

---
+++ src/main/java/kyu8/Kata081.java
@@ -0,0 +1,21 @@
+package kyu8;
+
+import java.util.Collections;
+import java.util.List;
+
+/*
+Kata: Sort My Textbooks
+Task: HELP! Jason can't find his textbook!
+It is two days before the test date, and Jason's textbooks are all out of order!
+Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+The sorting should NOT be case sensitive
+ */
+public class Kata081 {
+    public static List<String> sort(List<String> textbooks) {
+        //use sort() from Collections with the static field of String class to ensure case insensitivity
+        Collections.sort(textbooks, String.CASE_INSENSITIVE_ORDER);
+        return textbooks;
+    }
+
+}
