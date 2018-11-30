dfcc6cefac706861976441559f795c3801c0030d
Андрей Григорьев <greegav@gmail.com>
2018-11-30 16:05:23 +0200

    Kata091

---
+++ src/main/java/kyu8/Kata091.java
@@ -0,0 +1,11 @@
+package kyu8;
+/*
+Kata: Number toString
+Task: The code gives an error!
+
+public static final String a = 123.toString();
+Fix it!
+ */
+public class Kata091 {
+    public static final String a = "123".toString();
+}
---
+++ src/test/java/kyu8/Kata091Test.java
@@ -0,0 +1,12 @@
+package kyu8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Kata091Test {
+    @Test
+    public void test() {
+        assertTrue(Kata091.a instanceof String);
+    }
+}
