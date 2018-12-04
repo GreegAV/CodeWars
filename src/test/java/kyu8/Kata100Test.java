b065b36f5b0836acf31da2111ceacec698aefba1
Андрей Григорьев <greegav@gmail.com>
2018-12-04 12:33:10 +0200

    Kata100

---
+++ src/test/java/kyu8/Kata100Test.java
@@ -0,0 +1,13 @@
+package kyu8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Kata100Test {
+
+    @Test
+    public void basicTest() {
+        assertEquals("apple", Kata100.subtractSum(10));
+    }
+}
