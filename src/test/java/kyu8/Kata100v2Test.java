caaa4153b8a386a1a2f073f97301f850d95cab97
Андрей Григорьев <greegav@gmail.com>
2018-12-04 12:43:41 +0200

    Kata100v2

--- src/test/java/kyu8/Kata100v2Test.java
+++ src/test/java/kyu8/Kata100v2Test.java
@@ -4,20 +4,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class Kata100Test {
+public class Kata100v2Test {
 
     @Test
     public void test10() {
-        assertEquals("apple", Kata100.subtractSum(10));
+        assertEquals("apple", Kata100v2.subtractSum(10));
     }
 
     @Test
     public void test111() {
-        assertEquals("apple", Kata100.subtractSum(111));
+        assertEquals("apple", Kata100v2.subtractSum(111));
     }
 
     @Test
     public void test1234() {
-        assertEquals("apple", Kata100.subtractSum(1234));
+        assertEquals("apple", Kata100v2.subtractSum(1234));
     }
 }
