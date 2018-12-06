283a1b2de1b71357a0906b3bf216cac98056945b
Андрей Григорьев <greegav@gmail.com>
2018-12-06 14:26:31 +0200

    Kata105

--- src/main/java/kyu8/Kata105.java
+++ src/main/java/kyu8/Kata105.java
@@ -1,5 +1,7 @@
 package kyu8;
 
+import java.util.Arrays;
+
 /*
 Kata: Pythagorean Triple
 Task: Given an array of 3 integers a, b and c, determine if they form a pythagorean triple.
@@ -18,9 +20,8 @@ For Python: return True or False
  */
 public class Kata105 {
     public int pythagoreanTriple(int[] triple) {
-        int a = triple[0] * triple[0];
-        int b = triple[1] * triple[1];
-        int c = triple[2] * triple[2];
-        return ((a + b == c) || (a + c == b) || (b + c == a)) ? 1 : 0;
+        Arrays.sort(triple);
+        return (triple[0] * triple[0] + triple[1] * triple[1] == triple[2] * triple[2]) ? 1 : 0;
+
     }
 }
