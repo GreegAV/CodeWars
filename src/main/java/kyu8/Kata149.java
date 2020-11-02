82d36c0a0979b9c393207e458b83e721a4b2ee56
Андрей Григорьев <greegav@gmail.com>
2020-11-02 17:32:56 +0200

    Kata149

---
+++ src/main/java/kyu8/Kata149.java
@@ -0,0 +1,29 @@
+package kyu8;
+/*
+Kata: Grasshopper - Messi Goals
+
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+1) Create these three variables and store the appropriate values using the table above:
+
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+2) Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+ */
+public class Kata149 {
+    public static int laLigaGoals=43;
+    public static int championsLeagueGoals=10;
+    public static int copaDelReyGoals=5;
+
+    public static int totalGoals=laLigaGoals+championsLeagueGoals+copaDelReyGoals;
+}
---
+++ src/test/java/kyu8/Kata149Test.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class Kata149Test {
+    @Test
+    public void BasicTests() {
+        assertEquals(58, Kata149.totalGoals);
+    }
+}
