e4bada75286520de55171d52feed5211c2548acb
Андрей Григорьев <greegav@gmail.com>
2020-11-03 17:35:07 +0200

    Kata153

---
+++ src/test/java/kyu8/Kata153Test.java
@@ -0,0 +1,16 @@
+package kyu8;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Kata153Test {
+
+    final double epsilon = 0.0000001;
+
+    @Test public void pulledTest1() {
+        assertEquals(0.6, Kata153.guessBlue(5, 5, 2, 3), epsilon);
+    }
+
+    @Test public void pulledTest2() {
+        assertEquals(0.2, Kata153.guessBlue(5, 7, 4, 3), epsilon);
+    }
+}
