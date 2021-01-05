cf20133a716bc0f054bae26dec717c5ab78116fb
Андрей Григорьев <greegav@gmail.com>
2021-01-05 11:07:45 +0200

    Kata175

---
+++ src/main/java/kyu8/Kata175.java
@@ -0,0 +1,31 @@
+package kyu8;
+
+/*
+Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+For example, a multiplication table (string) for number == 5 looks like below:
+
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+P. S. You can use \n in string to jump to the next line.
+
+Note: newlines should be added between rows, but there should be no trailing newline at the end.
+ */
+public class Kata175 {
+    public static String multiTable(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 9; i++) {
+            sb.append(i).append(" * ").append(num).append(" = ").append(i * num).append("\n");
+        }
+        sb.append(10 + " * ").append(num).append(" = ").append(10 * num);
+        return sb.toString(); // good luck
+    }
+}
